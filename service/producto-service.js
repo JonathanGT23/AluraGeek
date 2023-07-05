5c197818c0348b71e79f17a4264a8d4ff734cb07
Jonathan González T <132779706+JonathanGT23@users.noreply.github.com>
2023-07-04 20:29:13 -0600

    Update producto-service.js

--- service/producto-service.js
+++ service/producto-service.js
@@ -1,11 +1,11 @@
 const listaProductos = () =>{
-    return fetch ("https://github.com/JonathanGT23/AluraGeek/main/db.json").then((respuesta) => respuesta.json())
+    return fetch ("https://alurageek-jonathangt23.onrender.com/productos").then((respuesta) => respuesta.json())
 };
 
 
 
 const agregarProducto = (imagen,nombre,precio,categoria,descripcion)=>{
-    return fetch("https://github.com/JonathanGT23/AluraGeek/main/db.json",{
+    return fetch("https://alurageek-jonathangt23.onrender.com/productos",{
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -16,17 +16,17 @@ const agregarProducto = (imagen,nombre,precio,categoria,descripcion)=>{
 }
 
 const eliminarProducto = (id) =>{
-    return fetch(`https://github.com/JonathanGT23/AluraGeek/main/db.json/${id}` ,{
+    return fetch(`https://alurageek-jonathangt23.onrender.com/productos/${id}` ,{
         method: "DELETE"
     })
 }
 
 const datosProducto = (id) =>{
-    return fetch(`https://github.com/JonathanGT23/AluraGeek/main/db.json/${id}`).then((respuesta) => respuesta.json())
+    return fetch(`https://alurageek-jonathangt23.onrender.com/productos/${id}`).then((respuesta) => respuesta.json())
 }
 
 const editarProducto = (imagen,nombre,precio,categoria,descripcion,id) =>{
-    return fetch(`https://github.com/JonathanGT23/AluraGeek/main/db.json/${id}`,{
+    return fetch(`https://alurageek-jonathangt23.onrender.com/productos/${id}`,{
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
